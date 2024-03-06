5bab30a977635519288cbf0a026523ea725279b7
elexispd <promisedeco24@gmail.com>
2024-03-06 23:27:16 +0100

    seperated post & comment sections

---
+++ resources/views/includes/comments.blade.php
@@ -0,0 +1,35 @@
+<hr>
+
+<div class="d-flex align-items-start" style="margin-left:15px;">
+    <img style="width:35px" class="me-2 avatar-sm rounded-circle"
+        src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=Luigi"
+        alt="Luigi Avatar">
+    <div class="w-100">
+        <div class="d-flex justify-content-between">
+            <h6 class="">Luigi
+            </h6>
+
+            <small class="fs-6 fw-light text-muted"> 3 hour
+                ago</small>
+        </div>
+        <p class="fs-6 mt-3 fw-light">
+            and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
+            Evil)
+            by
+            Cicero, written in 45 BC. This book is a treatise on the theory of ethics,
+            very
+            popular during the Renaissan
+        </p>
+        <div class="d-flex justify-content-end">
+            <div>
+                <a href="#" class="fw-light text-decoration-none fs-6">
+                    <span class="fas fa-heart"></span> 100
+                </a>
+                <a href="#" class="fw-light text-decoration-none me-1 fs-6">
+                    <span class="fas fa-thumbs-down"></span> 20
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
